641b171aeac02528c40bd63a61967724129e3c6f
s10-lee <quicksilverinveins@gmail.com>
2021-03-26 16:37:25 +0300

    test amazon pipeline by push

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Test Amazon pipeline.'
-
-
-if __name__ == '__main__':
-    app.run()
