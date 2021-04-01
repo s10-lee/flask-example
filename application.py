85e7815e0126410c8381423101c8ee2ef39bf366
s10-lee <quicksilverinveins@gmail.com>
2021-04-01 13:16:52 +0300

    app to application

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Application no debug !', 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
